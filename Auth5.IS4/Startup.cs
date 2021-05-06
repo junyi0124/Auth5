847b2db86e1658ab5881764a2b3876dd2cf0fb4a
junyi <junyi0124@msn.com>
2021-05-06 21:46:44 +0800

    aa

--- Auth5.IS4/Startup.cs
+++ Auth5.IS4/Startup.cs
@@ -12,8 +12,6 @@ namespace Auth5.IS4
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
@@ -21,7 +19,6 @@ namespace Auth5.IS4
             services.AddIdentityServer();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
